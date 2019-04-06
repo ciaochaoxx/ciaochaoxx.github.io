345a304e88cd6f2b2489bb1c8c6246b692231f75
Yanchao Li <47365423+ciaochaoxx@users.noreply.github.com>
2019-04-06 22:17:16 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$("#link1").click(function(){ $("#hiraganachart").toggle();
+});
+$("#link2").click(function(){ $("#katakanachart").toggle();
+});
+$("#link3").click(function(){ $("#kanjichart").toggle();
+});
+$("#link4").click(function(){ $("#usefulphrases").toggle();
+});
+$("#link5").click(function(){ $("#culturepart").toggle();
+});
